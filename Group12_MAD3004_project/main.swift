ff2d6ce9231ac4d8a71235f1d948b2c27a1328c9
MacStudent <macstudent@20x-imacxx-m-439.local>
2018-07-26 16:05:03 -0400

    Initial Commit

---
+++ Group12_MAD3004_project/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Group12_MAD3004_project
+//
+//  Created by MacStudent on 2018-07-26.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
